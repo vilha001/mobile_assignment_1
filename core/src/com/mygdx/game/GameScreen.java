c3492080c3e76dbfaeeaedf91381cd86f96f0810
vilha001@mymail.unisa.edu.au <38933823+vilha001@users.noreply.github.com>
2019-04-20 20:31:39 +0930

    finished main features

--- core/src/com/mygdx/game/GameScreen.java
+++ core/src/com/mygdx/game/GameScreen.java
@@ -1,7 +1,6 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
@@ -41,6 +40,11 @@ public class GameScreen implements Screen {
     ArrayList<Sprite> bricks2 = new ArrayList<Sprite>();
     ArrayList<Sprite> bricks3 = new ArrayList<Sprite>();
     ArrayList<Sprite> bricks4 = new ArrayList<Sprite>();
+    ArrayList<Sprite> destroyed = new ArrayList<Sprite>();
+    ArrayList<Integer> bricks1Destroyed = new ArrayList<Integer>();
+    ArrayList<Integer> bricks2Destroyed = new ArrayList<Integer>();
+    ArrayList<Integer> bricks3Destroyed = new ArrayList<Integer>();
+    ArrayList<Integer> bricks4Destroyed = new ArrayList<Integer>();
 
 
     TextButton retryButton;;
@@ -48,6 +52,8 @@ public class GameScreen implements Screen {
     TextButton gameOver;
     TextButton paused;
     TextButton pauseButton;
+    TextButton resumeButton;
+    TextButton gameWin;
     BitmapFont mainFont;
     TextButton.TextButtonStyle textButtonStyle;
 
@@ -112,7 +118,24 @@ public class GameScreen implements Screen {
         pauseButton.setTransform(true);
 
         pauseButton.setScale(1f, 1f);
+
+        resumeButton = new TextButton("Resume", textButtonStyle);
+        //sets btn location
+        resumeButton.setPosition(50f, Gdx.graphics.getHeight()/2 - 150f);
+        //sets btn size
+        resumeButton.setTransform(true);
+        resumeButton.setScale(5f, 5f);
         //end pause state
+
+        //win state
+        gameWin = new TextButton("YOU WIN", textButtonStyle);
+        //sets btn location
+        gameWin.setPosition((Gdx.graphics.getWidth() /2 - 225f), Gdx.graphics.getHeight()/2 + 50f);
+        //sets btn size
+        gameWin.setTransform(true);
+
+        gameWin.setScale(7f, 7f);
+        //end of win state
 
 
 
@@ -228,23 +251,52 @@ public class GameScreen implements Screen {
 
         //checks collision for bricks
         for(int i = 0; i < 20; i++){
-            if(ball.getBoundingRectangle().overlaps(bricks1.get(i).getBoundingRectangle())){
-                directionY = false;
+            //checks if it has been hit before, if so don't check collision
+            if(bricks1Destroyed.contains(i)){
+
+            }else{
+                if(ball.getBoundingRectangle().overlaps(bricks1.get(i).getBoundingRectangle())){
+                    directionY = false;
+                    destroyed.add(bricks1.get(i));
+                    bricks1Destroyed.add(i);
+                }
+            }
+
+        }
+
+        for(int i = 0; i < 20; i++){
+            if(bricks2Destroyed.contains(i)){
+
+            }else{
+                if(ball.getBoundingRectangle().overlaps(bricks2.get(i).getBoundingRectangle())){
+                    directionY = false;
+                    destroyed.add(bricks2.get(i));
+                    bricks2Destroyed.add(i);
+                }
             }
         }
+
         for(int i = 0; i < 20; i++){
-            if(ball.getBoundingRectangle().overlaps(bricks2.get(i).getBoundingRectangle())){
-                directionY = false;
+            if(bricks3Destroyed.contains(i)){
+
+            }else{
+                if(ball.getBoundingRectangle().overlaps(bricks3.get(i).getBoundingRectangle())){
+                    directionY = false;
+                    destroyed.add(bricks3.get(i));
+                    bricks3Destroyed.add(i);
+                }
             }
         }
+
         for(int i = 0; i < 20; i++){
-            if(ball.getBoundingRectangle().overlaps(bricks3.get(i).getBoundingRectangle())){
-                directionY = false;
-            }
-        }
-        for(int i = 0; i < 20; i++){
-            if(ball.getBoundingRectangle().overlaps(bricks4.get(i).getBoundingRectangle())){
-                directionY = false;
+            if(bricks4Destroyed.contains(i)){
+
+            }else{
+                if(ball.getBoundingRectangle().overlaps(bricks4.get(i).getBoundingRectangle())){
+                    directionY = false;
+                    destroyed.add(bricks4.get(i));
+                    bricks4Destroyed.add(i);
+                }
             }
         }
 
@@ -270,13 +322,7 @@ public class GameScreen implements Screen {
 
         if(Gdx.input.getX() < 50 && Gdx.input.getY() < 100 && Gdx.input.getY() > 75){
             if(Gdx.input.isTouched()){
-                //had to add delay otherwise multiclick registered
-                Timer.schedule(new Timer.Task() {
-                    @Override
-                    public void run() {
-                        pause = true;
-                    }
-                },  (float)0.1);
+                pause = true;
             }
         }
 
@@ -298,20 +344,32 @@ public class GameScreen implements Screen {
         ball.setSize(16,16);
 
 
-        //checks if paused, gameover or not started
-        if(pause || gameOverState || playing == false ){
+        //checks if paused, gameover, success or not started or pretty much anything that needs the
+        // update stopped as none of them can be accessed at the same time
+        //for quick...er testing of win state remove all but brick4Destroyed the lowest row
+        //why did i make the x direction after collision random :(
+        if(pause || gameOverState || playing == false
+                || bricks4Destroyed.size() > 19 && bricks3Destroyed.size() >
+                19 && bricks2Destroyed.size() > 19 && bricks1Destroyed.size() > 19 ){
             if(pause){
-                paused.draw(game.batch,10);
-                //if touch screen resume game
-                if(Gdx.input.isTouched()){
-                    //had to add delay otherwise multiclick registered
-                    Timer.schedule(new Timer.Task() {
-                        @Override
-                        public void run() {
-                            pause = false;
-                        }
-                    },1);
+                float exitX = Gdx.graphics.getWidth() /2 + 100f;
+                float playX = 50;
 
+                paused.draw(game.batch,10);
+                exitButton.draw(game.batch,10);
+                resumeButton.draw(game.batch, 10);
+
+                if(Gdx.input.getX() > exitX && Gdx.input.getX() < exitX + 130f
+                        && Gdx.input.getY() > 275 && Gdx.input.getY() < 330){
+                    if(Gdx.input.isTouched()){
+                        Gdx.app.exit();
+                    }
+                }
+                if(Gdx.input.getX() > playX && Gdx.input.getX() < playX + 215f
+                        && Gdx.input.getY() > 275 && Gdx.input.getY() < 330){
+                    if(Gdx.input.isTouched()){
+                        pause = false;
+                    }
                 }
             }else if(gameOverState) {
                 gameOver.draw(game.batch,10);
@@ -327,6 +385,7 @@ public class GameScreen implements Screen {
                 if(Gdx.input.getX() > playX && Gdx.input.getX() < playX + 215f
                         && Gdx.input.getY() > 275 && Gdx.input.getY() < 330){
                     if(Gdx.input.isTouched()){
+                        //starts game again
                         game.setScreen(new GameScreen(game));
                     }
                 }
@@ -339,7 +398,7 @@ public class GameScreen implements Screen {
                     }
                 }
 
-            } else{
+            } else if(playing == false){
                 //if game not started
                 moveX = Gdx.input.getX() + playerSprite.getWidth()/2;
                 moveY = 75;
@@ -360,6 +419,33 @@ public class GameScreen implements Screen {
                         }
                     }, (float)0.1);
                 }
+            } else{
+                //mostly same as gameover
+
+                gameWin.draw(game.batch,10);
+                retryButton.draw(game.batch,10);
+                exitButton.draw(game.batch,10);
+
+                float playX = 50;
+                //y value was not corrosponding to actual location?
+
+                float exitX = Gdx.graphics.getWidth() /2 + 100f;
+
+                //gets button location
+                if(Gdx.input.getX() > playX && Gdx.input.getX() < playX + 215f
+                        && Gdx.input.getY() > 275 && Gdx.input.getY() < 330){
+                    if(Gdx.input.isTouched()){
+                        game.setScreen(new GameScreen(game));
+                    }
+                }
+
+                //gets button location
+                if(Gdx.input.getX() > exitX && Gdx.input.getX() < exitX + 130f
+                        && Gdx.input.getY() > 275 && Gdx.input.getY() < 330){
+                    if(Gdx.input.isTouched()){
+                        Gdx.app.exit();
+                    }
+                }
             }
 
         } else{
@@ -369,32 +455,54 @@ public class GameScreen implements Screen {
 
         int brickXLocation1 = 0;
         for(int i = 0; i < 20; i++ ){
-            bricks1.get(i).setPosition(brickXLocation1, 464);
-            bricks1.get(i).draw(game.batch);
-            brickXLocation1 += 32;
+            //checks if brick has been hit before
+            if(destroyed.contains(bricks1.get(i))){
+                //updates location for other bricks but does not draw
+                brickXLocation1 += 32;
+            } else{
+                //draws like normal
+                bricks1.get(i).setPosition(brickXLocation1, 464);
+                bricks1.get(i).draw(game.batch);
+                brickXLocation1 += 32;
+            }
+
         }
 
         int brickXLocation2 = 0;
         for(int i = 0; i < 20; i++ ){
-            bricks2.get(i).setPosition(brickXLocation2, 448);
-            bricks2.get(i).draw(game.batch);
-            brickXLocation2 += 32;
+            if(destroyed.contains(bricks2.get(i))){
+                brickXLocation2 += 32;
+            }else{
+                bricks2.get(i).setPosition(brickXLocation2, 448);
+                bricks2.get(i).draw(game.batch);
+                brickXLocation2 += 32;
+            }
+
         }
 
         int brickXLocation3 = 0;
         for(int i = 0; i < 20; i++ ){
-            bricks3.get(i).setPosition(brickXLocation3, 432);
-            bricks3.get(i).draw(game.batch);
-            brickXLocation3 += 32;
+            if(destroyed.contains(bricks3.get(i))){
+                brickXLocation3 += 32;
+            }else{
+                bricks3.get(i).setPosition(brickXLocation3, 432);
+                bricks3.get(i).draw(game.batch);
+                brickXLocation3 += 32;
+            }
+
         }
 
         int brickXLocation4 = 0;
         for(int i = 0; i < 20; i++ ){
-            bricks4.get(i).setPosition(brickXLocation4, 416);
-            bricks4.get(i).draw(game.batch);
-            brickXLocation4 += 32;
-        }
+            if(destroyed.contains(bricks4.get(i))){
+                brickXLocation4 += 32;
+            }else{
+                bricks4.get(i).setPosition(brickXLocation4, 416);
+                bricks4.get(i).draw(game.batch);
+                brickXLocation4 += 32;
+            }
 
+        }
 
         pauseButton.draw(game.batch, 10);
         playerSprite.draw(game.batch);
